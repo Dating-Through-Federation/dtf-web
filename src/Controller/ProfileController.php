187629bdfff6059247921f640e9b8e7d1c294151
Jason Marshall <jasonmarshallselene@gmail.com>
2025-01-05 15:39:04 -0500

    getting ready for status settings

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\ProfileType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -13,10 +15,15 @@ class ProfileController extends AbstractController
 {
     #[IsGranted('ROLE_USER')]
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ProfileType::class, $this->getUser()->getProfile());
-        // dd($form);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($profile);
+            $entityManager->flush();
+        }
 
         return $this->render('profile/index.html.twig', [
             'form' => $form,
